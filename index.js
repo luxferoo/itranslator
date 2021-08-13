65292ba82f72961acd550c51f31d46455cfda2ea
Imam Harir <Imam.Harir@adprod.local>
2021-08-13 17:20:26 +0100

    use jest for running tests

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 let config;
 
 module.exports = {
-    trans(string, configuration) {
-        const _config = {
-            ...config,
-            ...configuration
+  trans(string, configuration) {
+    const _config = {
+      ...config,
+      ...configuration,
+    };
+    checkConfig(_config);
+    let { vars, source } = _config;
+    try {
+      const chunks = string.split(".");
+      for (let i in chunks) {
+        source = source[chunks[i]];
+        if (chunks.length - 1 === +i) {
+          vars &&
+            Object.entries(vars).forEach((entry) => {
+              source = source.replace("%" + entry[0] + "%", entry[1]);
+            });
         }
-        checkConfig(_config);
-        let { vars, source } = _config;
-        try {
-            const chunks = string.split('.');
-            for (let i in chunks) {
-                source = source[chunks[i]];
-                if (chunks.length - 1 === +i) {
-                    vars && Object.entries(vars).forEach(entry => {
-                        source = source.replace('%' + entry[0] + '%', entry[1]);
-                    })
-                }
-            }
-        } catch (e) {
-            return string;
-        }
-        return source === undefined ? string : source;
-    },
-    config(configuration) {
-        checkConfig(configuration);
-        config = configuration;
+      }
+    } catch (e) {
+      return string;
     }
+    return source === undefined ? string : source;
+  },
+  config(configuration) {
+    checkConfig(configuration);
+    config = configuration;
+  },
 };
 
 function checkConfig(config) {
-    if (typeof config !== "object") {
-        throw new Error("configuration must be of type json object.")
+  if (typeof config !== "object") {
+    throw new Error("configuration must be of type json object.");
+  }
+
+  Object.entries(config).forEach((entry) => {
+    if (!["vars", "source"].includes(entry[0])) {
+      throw new Error(
+        entry[0] +
+          " is not a configuration parameter. Only 'vars' and 'source' are supported."
+      );
     }
 
-    Object.entries(config).forEach(entry => {
-        if (!["vars", "source"].includes(entry[0])) {
-            throw new Error(entry[0] + " is not a configuration parameter. Only 'vars' and 'source' are supported.")
-        }
-
-        if (["vars", "source"].includes(entry[0]) && typeof entry[1] !== "object") {
-            throw new Error(entry[0] + " must be of type json object.")
-        }
-    })
+    if (["vars", "source"].includes(entry[0]) && typeof entry[1] !== "object") {
+      throw new Error(entry[0] + " must be of type json object.");
+    }
+  });
 }
